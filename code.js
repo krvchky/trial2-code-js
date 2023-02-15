caecbd616445d5426dfd5f2f686553d8774202ba
krvchky <kravchukyana05@gmail.com>
2023-02-15 16:29:16 +0100

    new

---
+++ code.js
@@ -0,0 +1,100 @@
+// indexOF and lastIndexOf
+
+// const word = "banana"
+// console.log(word.indexOf("a")); //2
+// console.log(word.lastIndexOf("a")); //1
+
+//toLowerCase()) and toUpperCase())
+
+// const c = "hcjcPXC"
+// console.log(c.toLowerCase())
+// console.log(c.toUpperCase())
+
+//REPLASE()
+
+// const myName = "like banana"
+// const newName = myName.replace("banana", "dogs")
+// console.log(newName)
+
+//SPLICE()
+
+// const word5 = "javascript"
+// console.log(word5.slice(4)) // java
+
+//SPLIT()
+const surname = "Kravchuk"
+console.log(surname.split("")) //['K', 'r', 'a', 'v', 'c', 'h', 'u', 'k']
+
+const string = '1-!2-!3-!4-!5-!6-!7'
+console.log(string.split('!')) //['1-', '2-', '3-', '4-', '5-', '6-', '7']
+
+// JOIN()
+
+const allWords = ["Яна", "ненавидить", "коли", "ій" ,"хтось", "щось", "забороняє"]
+console.log(allWords.join("!"))//Яна!ненавидить!коли!ій!хтось!щось!забороняє
+console.log(allWords.join("-"))//Яна-ненавидить-коли-ій-хтось-щось-забороняє
+
+//INCLUDES()
+const newNames = ["moolly","polly"]
+console.log(newNames.includes("moolly")) //true
+console.log(newNames.includes("yana")) //false
+//ЛОГІЧНІ ОПЕРАТОРИ
+
+// I - &&
+// або - ||
+// не - !
+
+
+// IF ELSE 
+
+// const number1 = Number(prompt("enter a number"))
+// console.log(number1)
+// const number2 = Number(prompt("enter a number"))
+// console.log(number2)
+
+// if (number1 > number2) {
+//     console.log("im here!")
+// } else {
+//     console.log("sorry!")
+// }
+
+
+// SWITCH
+
+// const number6 = Number(prompt("Enter a num between 0 and 3"))
+
+// switch (number6) {
+//     case 0:
+//         console.log('You entered 0')
+//         break;
+//     case 1:
+//         console.log('You entered 1')
+//         break;
+//     case 2:
+//         console.log('You entered 2')
+//         break;
+//     case 3:
+//         console.log('You entered 3')
+//         break;  
+//     case 4:
+//         console.log('You entered 4')
+//         break;
+// }
+
+// for 
+
+// let word1 = '' 
+
+// for (let i = 0; i <=3; i+=1) {
+//     console.log('previous string', word1)
+//     console.log(i)
+//     word1 = word1 + i
+//     console.log('next string', word1)
+// } 
+
+// const clients = [100]
+// for (const client of clients){
+//     console.log(client +1)
+// }
+
+
