d8afa542ae6242b6eb5d00f98a6d865134c8b503
krvchky <kravchukyana05@gmail.com>
2023-02-17 16:38:01 +0100

    У

--- code.js
+++ code.js
@@ -18,57 +18,57 @@
 // console.log(word5.slice(4)) // java
 
 //SPLIT()
-const surname = "Kravchuk";
-console.log(surname.split("")); //['K', 'r', 'a', 'v', 'c', 'h', 'u', 'k']
+// const surname = "Kravchuk";
+// console.log(surname.split("")); //['K', 'r', 'a', 'v', 'c', 'h', 'u', 'k']
 
-const str = "The quick brown fox jumps over the lazy dog.";
-const words = str.split(" ");
-console.log(words[3]); // "fox"
+// // const str = "The quick brown fox jumps over the lazy dog.";
+// // const words = str.split(" ");
+// // console.log(words[3]); // "fox"
 
-const chars = str.split("");
-console.log(chars[8]); //k
+// const chars = str.split("");
+// console.log(chars[8]); //k
 
-const string = "1-!2-!3-!4-!5-!6-!7";
-console.log(string.split("!")); //['1-', '2-', '3-', '4-', '5-', '6-', '7']
+// const string = "1-!2-!3-!4-!5-!6-!7";
+// console.log(string.split("!")); //['1-', '2-', '3-', '4-', '5-', '6-', '7']
 
-// JOIN() СТАВИТЬ В МАСИВ СИМОВЛ ЯКИЙ МИ ВСТАВИЛИ В СОНСОЛЬ.ЛОГ
-const allWords = [
-  "Яна",
-  "ненавидить",
-  "коли",
-  "ій",
-  "хтось",
-  "щось",
-  "забороняє",
-];
-console.log(allWords.join("!")); //Яна!ненавидить!коли!ій!хтось!щось!забороняє
-console.log(allWords.join("-")); //Яна-ненавидить-коли-ій-хтось-щось-забороняє
+// // JOIN() СТАВИТЬ В МАСИВ СИМОВЛ ЯКИЙ МИ ВСТАВИЛИ В СОНСОЛЬ.ЛОГ
+// const allWords = [
+//   "Яна",
+//   "ненавидить",
+//   "коли",
+//   "ій",
+//   "хтось",
+//   "щось",
+//   "забороняє",
+// ];
+// console.log(allWords.join("!")); //Яна!ненавидить!коли!ій!хтось!щось!забороняє
+// console.log(allWords.join("-")); //Яна-ненавидить-коли-ій-хтось-щось-забороняє
 
-//INCLUDES() ПЕРЕВІРЯЄ ЧИ ВКЛЮЧАЄ В СЕБЕ МАСИВ ЗНАЧЕННЯ
-const newNames = ["moolly", "polly"];
-console.log(newNames.includes("moolly")); //true
-console.log(newNames.includes("yana")); //false
+// //INCLUDES() ПЕРЕВІРЯЄ ЧИ ВКЛЮЧАЄ В СЕБЕ МАСИВ ЗНАЧЕННЯ
+// const newNames = ["moolly", "polly"];
+// console.log(newNames.includes("moolly")); //true
+// console.log(newNames.includes("yana")); //false
 
-//PUSH ДОДАЄ НА ПОЧАТОК МАСИВУ ЗНАЧЕННЯ
-const array = [1, 2];
-array.push(3, 4, 5);
-console.log(array); // 1 2 3 4 5
+// //PUSH ДОДАЄ НА ПОЧАТОК МАСИВУ ЗНАЧЕННЯ
+// const array = [1, 2];
+// array.push(3, 4, 5);
+// console.log(array); // 1 2 3 4 5
 
-const animals = ["pig", "monkey", "dog", "cat"];
-animals.push("frog", "bird");
-console.log(animals); //['pig', 'monkey', 'dog', 'cat', 'frog', 'bird']
+// const animals = ["pig", "monkey", "dog", "cat"];
+// animals.push("frog", "bird");
+// console.log(animals); //['pig', 'monkey', 'dog', 'cat', 'frog', 'bird']
 
-const array1 = [];
-array1.push(1, 2, 3, 4, 5);
-console.log(array1); //[1, 2, 3, 4, 5]
+// const array1 = [];
+// array1.push(1, 2, 3, 4, 5);
+// console.log(array1); //[1, 2, 3, 4, 5]
 
-//POP() ВИДАЛЯЄ ОСТАННІЙ ЕЛЕМЕНТ МАСИВУ
-const emptyArray = [1, 2, 3, 4, 5, 6];
-emptyArray.pop();
-console.log(); //[1, 2, 3, 4, 5]
+// //POP() ВИДАЛЯЄ ОСТАННІЙ ЕЛЕМЕНТ МАСИВУ
+// const emptyArray = [1, 2, 3, 4, 5, 6];
+// emptyArray.pop();
+// console.log(); //[1, 2, 3, 4, 5]
 
-const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
-console.log(plants.pop()); //tomato
+// const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+// console.log(plants.pop()); //tomato
 
 //ЛОГІЧНІ ОПЕРАТОРИ
 // I - &&
@@ -108,37 +108,47 @@ console.log(plants.pop()); //tomato
 //         break;
 // }
 
-// FOR
-// let word1 = ''
+// const name = prompt("Whats your name?")
+// console.log("Hello " + name);
 
-// for (let i = 0; i <=3; i+=1) {
-//     console.log('previous string', word1)
-//     console.log(i)
-//     word1 = word1 + i
-//     console.log('next string', word1)
+// const likeCats = confirm("Do you like cats?");
+// if (likeCats) {
+//   console.log("You are a cool cat!");
+// } else {
+//   console.log("Yeah,u are still cool");
+// }
+ 
+//ГРА ШИБЕНИЦЯ 
+// const words = ["javascript", "monkey", "amazing", "puncake"];
+// const word = words[Math.floor(Math.random()*words.length)];
+
+// let answerArray = []
+// for (let i = 0; i < words.length; i++ ) {
+//   answerArray[i] = "_";
 // }
 
-//FOR OF
-// const clients = [100]
-// for (const client of clients){
-//     console.log(client +1)
+// let remainingLetters = word.length;
+
+// while (remainingLetters >0) {
+//   alert (answerArray.join(" "))
+
+// let guess = prompt("Guess a letter or click Cansel to stop playing.");
+// if (guess === null){
+//   break;
+// } else if (guess.length !==1) {
+//   alert ("Please enter a single letter") 
+// } else {
+//   for (let j = 0; j <word.length;j ++) {
+//     if (word[j] === guess){
+//       answerArray[j] = guess;
+//       remainingLetters--;
+//     }
+//   }
 // }
 
-// function isNamberExist (array,number7){
-//     const result = array.includes(number7)
-//     console.log(result)
-//     return result
 // }
-// isNamberExist([1,2,3,4,5,6],7)
-// // isNamberExist([1,2,3,4,5,6],2)
 
-// function time (){
-//     const max = 200;
-//     const min = 50;
-//     let avarageTime = Math.floor(min + Math.random() * (max + 1 - min ))
-//     console.log(avarageTime);
-// }
-// time()
-// time()
-// time()
-// time()
+// alert(answerArray.join(" "));
+// alert("Good job. The answer was " + word);
+
+6
---
+++ cycle.js
@@ -0,0 +1,84 @@
+// FOR
+// let word1 = ''
+// for (let i = 0; i <=3; i+=1) {
+//     console.log('previous string', word1)
+//     console.log(i)
+//     word1 = word1 + i
+//     console.log('next string', word1)
+// }
+
+// for (var sheepCounter = 0; sheepCounter < 10; sheepCounter++) {
+//   console.log("I have counted " + sheepCounter + " sheep!");
+//   //I have counted 0 sheep!
+//   //I have counted 1 sheep!
+//   //I have counted 2 sheep!
+//   //I have counted 3 sheep!
+//   //I have counted 4 sheep!
+//   //I have counted 5 sheep!
+//   //I have counted 6 sheep!
+//   //I have counted 7 sheep!
+//   //I have counted 8 sheep!
+//   //I have counted 9 sheep!
+// }
+// console.log("Zzzzzzzzzzzzzz"); //ZZzzzzzzzzzz
+
+// let timesSayHello = 3;
+// for (let i = 0; i < timesSayHello; i ++) {
+//   console.log("Hello"); //3 Hello
+// }
+
+// let animals1 = ["dog","frog","flamingo","camel","bear","lion"]
+// for (let i = 0; i < animals1.length; i++) {
+//     console.log("This zoo contains a " + animals1[i] + "."); //This zoo contains a dog.
+//     // This zoo contains a frog.
+//     // This zoo contains a flamingo.
+//     // This zoo contains a camel.
+//     // This zoo contains a bear.
+//     // This zoo contains a lion.
+// }
+
+// let surname = "Nick";
+// for (let i = 0; i < surname.length; i++) {
+//   console.log("My name contsins the letter " + surname[i] + ".");
+//   // My name contsins the letter N.
+//   // My name contsins the letter i.
+//   // My name contsins the letter c.
+//   // My name contsins the letter k.
+// }
+
+
+// let animals = ["dog","frog","crocodile","cat"];
+// for (let i = 0; i < animals.length; i++) {
+//     console.log( "Awesome " + animals[i]) 
+//     //Awesome dog,
+//     //Awesome frog,
+//     //Awesome crocodile,
+//     //Awesome cat
+// }
+
+//FOR OF
+// const clients = [100]
+// for (const client of clients){
+//     console.log(client +1) //101
+// }
+
+// function isNamberExist (array,number7){
+//     const result = array.includes(number7)
+//     console.log(result)
+//     return result
+// }
+// isNamberExist([1,2,3,4,5,6],7) //false
+// isNamberExist([1,2,3,4,5,6],2) //true
+
+// function time (){
+//     const max = 200;
+//     const min = 50;
+//     let avarageTime = Math.floor(min + Math.random() * (max + 1 - min ))
+//     console.log(avarageTime); //random time 4th times
+// }
+// time()
+// time()
+// time()
+// time()
+
+
