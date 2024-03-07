2be74ff484646dde339c391bb70b800af995039b
krvchky <kravchukyana05@gmail.com>
2024-03-07 09:20:24 +0100

    cvbnm

--- code.js
+++ code.js
@@ -13,13 +13,57 @@
 
 
 
-let weight = '88,3'
-let height = '1.75'
-weight = Number(weight.replace(',','.'))
+// let weight = '88,3'
+// let height = '1.75'
+// weight = Number(weight.replace(',','.'))
 
-height = Number(height)
-// console.log(typeof height)
+// height = Number(height)
+// // console.log(typeof height)
 
-// const bmi = (weight / Math.pow(height, 2))
-const bmi = Number((weight / height ** 2).toFixed(1))
-console.log(bmi)
+// // const bmi = (weight / Math.pow(height, 2))
+// const bmi = Number((weight / height ** 2).toFixed(1))
+// console.log(bmi)
+
+
+const result = prompt("How old are u?");
+
+const isResultCorrect = result === "18";
+console.log(isResultCorrect ? "Yes" : "no");
+
+
+
+
+
+// const age = 19;
+// const city = "Rivne";
+
+// const string = `I'm ${age} years old. I live in ${city}`
+
+// console.log(string)
+
+//    let students = 100;
+
+//    students -= 50;
+
+//    console.log(students)
+
+// const value = 27.4;
+// console.log(Math.round(value)) //27  //ЗАОКРУГЛЮЄ дИВЛЯЧИСЬ ЯКА ЙИФРА ПІСЛЯ КОМИ 
+
+
+
+// const value = 27.9999;
+// console.log(Math.floor(value))  //27 // ДО МЕНШОГО
+
+
+// const value = 27.000001;
+// console.log(Math.ceil(value))  //28 // ДО БІЛЬШОГО
+
+
+
+// const companyName = 'Cyberdyne System';
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// console.log(message) // Cyber System has 200 bots in stock
+
